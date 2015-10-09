1e12c14bd457ab7cff39fcc14075587d2e29397f
Matt Fettke <matt@snapcontrol.com>
2015-10-09 12:41:40 +1030

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+workspace "RePlex"
+    configurations {"Debug", "Release"}
+    -- Use C++ as the target language for all builds
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    flags { "C++14" }
+
+    filter "configurations:Debug"
+        -- Add the preprocessor definition DEBUG to debug builds
+        defines { "DEBUG" }
+        -- Ensure symbols are bundled with debug builds
+        flags { "Symbols" }
+
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        -- Turn on compiler optimizations for release builds
+        optimize "On"
+
+    -- RePlex library
+    project "RePlex"
+        kind "SharedLib"
+        -- recursively glob .h and .cpp files in the lib directory
+        files { "lib/**.h", "lib/**.cpp" }
+
+    -- RePlex runtime
+    project "RePlexRuntime"
+        kind "ConsoleApp"
+        files { "runtime/**.h", "runtime/**.cpp" }
+        -- link the RePlexLib library at runtime
+        links { "RePlex" }
+        includedirs { "/lib/pub" }
+
---
+++ runtime/Main.cpp
@@ -0,0 +1,11 @@
+#include <RePlex.h>
+#include <iostream>
+
+int main()
+{
+    Foo foo;
+
+    std::cout << "The answer is " << foo.GetTheAnswer() << std::endl;
+
+    return 0;
+}
